11ee674f5e88635d702049ea7b81691261b60336
Leonardo Vettulo <leonardovettulo@gmail.com>
2019-09-23 15:35:00 -0300

    Added main modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ItemComponent } from "./pages/item/item.component";
+import { ItemsComponent } from "./pages/items/items.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "items", component: ItemsComponent },
+  { path: "item", component: ItemComponent },
+  { path: "**", pathMatch: "full", redirectTo: "items" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ItemsComponent } from './pages/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
